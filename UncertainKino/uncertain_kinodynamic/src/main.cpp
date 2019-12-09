7a10f3f314657332297c90eb12afea84856a577f
JJHbrams <30363119+JJHbrams@users.noreply.github.com>
2019-12-09 14:10:01 +0900

    main.cpp

--- UncertainKino/uncertain_kinodynamic/src/main.cpp
+++ UncertainKino/uncertain_kinodynamic/src/main.cpp
@@ -276,13 +276,13 @@ int main(int argc, char** argv) {
         ROS_INFO("No. of Waypoints = %d", NoPathPoints);
 
         //Save leadpath information as text
-        std::fstream fileout("/home/mrjohd/MotionPlanning_ws/src/UncertainKino/uncertain_kinodynamic/path_geo.txt", std::ios::out);
+        std::fstream fileout("src/UncertainKino/uncertain_kinodynamic/path_geo.txt", std::ios::out);
         p.printAsMatrix(fileout);
         //p.printAsMatrix(std::cout);
         fileout.close();
 
         //Read leadpath and divide it into constatnt distance
-        std::fstream filein("/home/mrjohd/MotionPlanning_ws/src/UncertainKino/uncertain_kinodynamic/path_geo.txt", std::ios::in);
+        std::fstream filein("src/UncertainKino/uncertain_kinodynamic/path_geo.txt", std::ios::in);
 
         char word_geo;
         char data_geo[MAX_COlUMN][4][MAX_WORDS]={0};
@@ -503,11 +503,11 @@ int main(int argc, char** argv) {
                 ompl::control::PathControl &path = simple_setup->getSolutionPath();
                 //path.printAsMatrix(std::cout);
 
-                std::fstream fileout("/home/mrjohd/MotionPlanning_ws/src/UncertainKino/uncertain_kinodynamic/path.txt", std::ios::out);
+                std::fstream fileout("src/UncertainKino/uncertain_kinodynamic/path.txt", std::ios::out);
                 path.printAsMatrix(fileout);
                 fileout.close();
 
-                std::fstream filein("/home/mrjohd/MotionPlanning_ws/src/UncertainKino/uncertain_kinodynamic/path.txt", std::ios::in);
+                std::fstream filein("src/UncertainKino/uncertain_kinodynamic/path.txt", std::ios::in);
 
                 char word;
                 char data[MAX_COlUMN][MAX_ROW][MAX_WORDS]={0};
